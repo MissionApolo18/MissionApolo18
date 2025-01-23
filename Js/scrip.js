7270176ae2b5159ebc783ec47134dd391dd492bb
Angela Atenea Larios Gutierrez. Tony <ateneagutierrez17@aragon.unam.mx>
2025-01-22 22:30:20 -0600

    Actualización a la sección de proyectos

--- Js/scrip.js
+++ Js/scrip.js
@@ -6,3 +6,33 @@ document.querySelectorAll('.card').forEach(card => {
         card.classList.toggle('open');
     });
 });
+
+const track = document.querySelector('.carousel-track');
+const cards = Array.from(track.children);
+const prevButton = document.querySelector('.prev-btn');
+const nextButton = document.querySelector('.next-btn');
+
+let currentIndex = 0;
+
+function updateCarousel(position) {
+    track.style.transform = `translateX(${position}px)`;
+}
+
+function moveToCard(index) {
+    const cardWidth = cards[0].getBoundingClientRect().width;
+    const position = -cardWidth * index;
+    updateCarousel(position);
+    currentIndex = index;
+}
+
+nextButton.addEventListener('click', () => {
+    if (currentIndex < cards.length - 1) {
+        moveToCard(currentIndex + 1);
+    }
+});
+
+prevButton.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        moveToCard(currentIndex - 1);
+    }
+});
